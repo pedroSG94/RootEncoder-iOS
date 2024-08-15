4bde2700d8bf94556493e01ce9b19a68da320a4c
pedroSG94 <pedrossgg1994@gmail.com>
2024-08-16 00:30:54 +0200

    allow record internal audio

--- RootEncoder/Sources/RootEncoder/encoder/input/video/ScreenManager.swift
+++ RootEncoder/Sources/RootEncoder/encoder/input/video/ScreenManager.swift
@@ -13,11 +13,19 @@ public class ScreenManager {
     private let screen = RPScreenRecorder.shared()
     private var running = false
     private let callbackVideo: GetCameraData?
-    private let callbackAudio: GetMicrophoneData?
+    private var callbackAudio: GetMicrophoneData?
+    private var muted = false
+    private(set) var recordingInternalAudio = false
     
     public init(callbackVideo: GetCameraData?, callbackAudio: GetMicrophoneData?) {
         self.callbackVideo = callbackVideo
         self.callbackAudio = callbackAudio
+        recordingInternalAudio = callbackAudio != nil
+    }
+    
+    public func setAudioCallback(callbackAudio: GetMicrophoneData?) {
+        self.callbackAudio = callbackAudio
+        recordingInternalAudio = callbackAudio != nil
     }
     
     public func start() {
@@ -25,14 +33,32 @@ public class ScreenManager {
             return
         }
         screen.isMicrophoneEnabled = false
-        screen.startCapture(handler: { (buffer, bufferType, error) in
+        screen.startCapture(handler: { (sampleBuffer, bufferType, error) in
             if let error = error {
                 print(error.localizedDescription)
             }
             if (bufferType.rawValue == RPSampleBufferType.video.rawValue) {
-                self.callbackVideo?.getYUVData(from: buffer)
+                self.callbackVideo?.getYUVData(from: sampleBuffer)
             } else if (bufferType.rawValue == RPSampleBufferType.audioApp.rawValue) {
-                //self.callbackAudio?.getPcmData(buffer: buffer)
+                let ts = UInt64(Date().millisecondsSince1970)
+                
+                guard let description = sampleBuffer.formatDescription, let blockBuffer = CMSampleBufferGetDataBuffer(sampleBuffer) else {
+                    return
+                }
+                let format = AVAudioFormat(cmAudioFormatDescription: description)
+                
+                var length = 0
+                var dataPointer: UnsafeMutablePointer<Int8>?
+                
+                CMBlockBufferGetDataPointer(blockBuffer, atOffset: 0, lengthAtOffsetOut: nil, totalLengthOut: &length, dataPointerOut: &dataPointer)
+                
+                let buffer = AVAudioPCMBuffer(pcmFormat: format, frameCapacity: AVAudioFrameCount(format.sampleRate))
+                buffer?.frameLength = AVAudioFrameCount(length) / format.streamDescription.pointee.mBytesPerFrame
+                memcpy(buffer?.int16ChannelData?[0], dataPointer, length)
+                
+                if let buffer = buffer {
+                    self.callbackAudio?.getPcmData(frame: PcmFrame(buffer: buffer.mute(enabled: self.muted), ts: ts, time: sampleBuffer.presentationTimeStamp))
+                }
             }
         }, completionHandler: { (error) in
             print(error?.localizedDescription ?? "")
@@ -41,7 +67,9 @@ public class ScreenManager {
     }
     
     public func stop() {
-        screen.stopCapture()
+        if screen.isRecording {
+            screen.stopCapture()
+        }
         running = false
     }
     
@@ -53,5 +81,17 @@ public class ScreenManager {
     public func getHeight() -> Int {
         let screenRect = UIScreen.main.nativeBounds
         return Int(screenRect.height)
+    }
+    
+    public func isMuted() -> Bool {
+        return muted
+    }
+    
+    public func mute() {
+        muted = true
+    }
+    
+    public func unmute() {
+        muted = false
     }
 }
