4158595ad1ccf75d5c8e0e1dd1350491b84c1e66
vitkuzmenko <kuzmenko.v.u@gmail.com>
2024-08-24 08:35:21 +0300

    Add torch support

--- RootEncoder/Sources/RootEncoder/library/base/CameraBase.swift
+++ RootEncoder/Sources/RootEncoder/library/base/CameraBase.swift
@@ -146,6 +146,11 @@ public class CameraBase {
         cameraManager.switchCamera()
     }
     
+    @discardableResult
+    public func setTorch(isOn: Bool) -> Bool {
+        cameraManager.setTorch(isOn: isOn)
+    }
+    
     public func isMuted() -> Bool {
         return microphone.isMuted()
     }
